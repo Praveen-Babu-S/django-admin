26421f21f553cc56a4a691aa878ecb83f1765cb0
Praveen Babu <praveenbbu22793@gmail.com>
2023-04-03 00:00:38 +0530

    add new views and urls

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse,HttpResponseNotFound
 # Create your views here.
+
+def serveRoutes(request,month):
+    if month == 'jan':
+        return HttpResponse('This is Jan')
+    elif month == 'feb':
+        return HttpResponse('This is Feb')
+    else:
+        return HttpResponseNotFound('No route found')
